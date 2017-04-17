be1a24223b39168f03d4bbd7df88e3ce05ae8ca7
Rodrigo Minaberrigaray <rodri.am93@gmail.com>
2017-04-16 21:03:26 -0300

    Así lo subo

--- src/main/java/modelo/Muber.java
+++ src/main/java/modelo/Muber.java
@@ -50,40 +50,56 @@ public class Muber {
 	}
 	
 	public void listarPasajeros() {
-		System.out.format("%-5s%-14s%-8s%n", "ID", "Nombre", "Créditos");
-		for( Usuario user : this.getUsuarios() ) {
-			if (user.isPasajero()) {
-				Pasajero pasajero = (Pasajero) user;
-				System.out.format("%-5s%-14s%-8s%n", pasajero.getIdUsuario(), pasajero.getNombre(), pasajero.getCreditos());
+		Set<Usuario> usuarios = this.getUsuarios();
+		if (!usuarios.isEmpty()) {
+			System.out.format("%-5s%-14s%-8s%n", "ID", "Nombre", "Créditos");
+			for( Usuario user : usuarios ) {
+				if (user.isPasajero()) {
+					Pasajero pasajero = (Pasajero) user;
+					System.out.format("%-5s%-14s%-8s%n", pasajero.getIdUsuario(), pasajero.getNombre(), pasajero.getCreditos());
+				}
 			}
 		}
+		else System.out.println("No hay pasajeros registrados");
 	}
 	
 	public void listarConductores() {
-		System.out.format("%-5s%-14s%-10s%n", "ID", "Nombre", "Venc. licencia");
-		for( Usuario user : this.getUsuarios() ) {
-			if (user.isConductor()) {
-				Conductor conductor = (Conductor) user;
-				System.out.format("%-5d%-14s%-10tD%n", conductor.getIdUsuario(), conductor.getNombre(), conductor.getFechaVencimientoLic());
+		Set<Usuario> usuarios = this.getUsuarios();
+		if (!usuarios.isEmpty()) {
+			System.out.format("%-5s%-14s%-10s%n", "ID", "Nombre", "Venc. licencia");
+			for( Usuario user : usuarios ) {
+				if (user.isConductor()) {
+					Conductor conductor = (Conductor) user;
+					System.out.format("%-5d%-14s%-10tD%n", conductor.getIdUsuario(), conductor.getNombre(), conductor.getFechaVencimientoLic());
+				}
 			}
 		}
+		else System.out.println("No hay conductores registrados");
 	}
 	
 	public void listarViajesAbiertos() {
+		Set<Viaje> viajes = this.getViajes();
+		int abiertos = 0;
 		System.out.format("%-5s%-15s%-15s%-10s%n", "ID", "Origen", "Destino", "Fecha");
-		for( Viaje viaje : this.getViajes() ) {
+		for( Viaje viaje : viajes ) {
 			if (!viaje.getFinalizado()) {
+				abiertos++;
 				System.out.format("%-5s%-15s%-15s%-10tc%n", viaje.getIdViaje(), viaje.getOrigen(), viaje.getDestino(), viaje.getFecha());
 			}
 		}
+		if (abiertos == 0) System.out.println("No hay viajes abiertos");
 	}
 	
 	private void printViajes(Conductor conductor) {
-		System.out.println("-----VIAJES-----");
-		System.out.format("%-5s%-15s%-15s%-10s%n", "ID", "Origen", "Destino", "Fecha");
-		for( Viaje viaje : conductor.getViajes() ) {
-			System.out.format("%-5s%-15s%-15s%-10tc%n", viaje.getIdViaje(), viaje.getOrigen(), viaje.getDestino(), viaje.getFecha());
+		Set<Viaje> viajes = conductor.getViajes();
+		if (!viajes.isEmpty()) {
+			System.out.println("-----VIAJES-----");
+			System.out.format("%-5s%-15s%-15s%-10s%n", "ID", "Origen", "Destino", "Fecha");
+			for( Viaje viaje : viajes ) {
+				System.out.format("%-5s%-15s%-15s%-10tc%n", viaje.getIdViaje(), viaje.getOrigen(), viaje.getDestino(), viaje.getFecha());
+			}
 		}
+		else System.out.println("El conductor no posee viajes registrados");
 	}
 	
 	public void getInfoConductor(int idConductor) {
--- src/main/java/utils/Consultas.java
+++ src/main/java/utils/Consultas.java
@@ -19,10 +19,27 @@ public class Consultas {
 		SessionFactory sf = cfg.buildSessionFactory();
 		Session session = sf.openSession();
 		
+		System.out.println("---------- LISTA PASAJEROS ----------");
 		Transaction tx = session.beginTransaction();
 		Muber muber = (Muber) session.createQuery("from Muber").uniqueResult();
+		muber.listarPasajeros();;
+		tx.rollback();
+		
+		System.out.println("---------- LISTA CONDUCTORES ----------");
+		tx = session.beginTransaction();
+		muber.listarConductores();;
+		tx.rollback();
+		
+		System.out.println("---------- VIAJES ABIERTOS ----------");
+		tx = session.beginTransaction();
+		muber.listarViajesAbiertos();;
+		tx.rollback();
+		
+		System.out.println("---------- INFO CONDUCTOR ----------");
+		tx = session.beginTransaction();
 		muber.getInfoConductor(2);
 		tx.rollback();
+		
 
 	}
 
